bc390f33f79e364714753bd1a7cca950d5843593
Annie <03.huynhphantramanh.9at2@gmail.com>
2024-08-28 23:08:33 +1000

    update YelpCamp

---
+++ app.js
@@ -0,0 +1,37 @@
+const exrpess = require('express');
+const app = exrpess();
+const path = require('path');
+const mongoose = require('mongoose');
+const Campground = require('./models/campground');
+const { title } = require('process');
+mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp', {
+  // useNewUrlParser: true,
+  // useCreateIndex: true,
+  // useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('Database connected');
+});
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+app.get('/makecampground', async (req, res) => {
+  const camp = new Campground({
+    title: 'My Backyard',
+    description: 'cheap camping',
+  });
+  await camp.save();
+  res.send(camp);
+});
+
+app.listen(3000, () => {
+  console.log('Serving on port 3000');
+});
